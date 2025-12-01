e0a675439eb885b13e04746630758789d67be314
Kushagra Bajpei <kushagrabajpeimbic@gmail.com>
2025-12-01 21:18:48 +0530

    send

---
+++ CHAPTER 2/02_datatypes.py
@@ -0,0 +1,15 @@
+a = 1     # a is an integer
+
+b = 5.22   # b is a floating point number
+
+c = "harry"  # c is a string
+
+d = False    # d is a boolean variable
+
+e = None     # e is a none type variable
+
+
+# variable name contain alphabets, digits, underscore
+# variable name only start with alphabet, underscore
+# variable name can't start with digit
+# No while Space is allowed to be used inside a variable name
---
+++ CHAPTER 2/problem6.py
@@ -0,0 +1,6 @@
+a = int(input("Enter Number: "))
+
+print("The square of the number is", a**2)
+print("The square of the number is", a*a)
+
+# print("The square of the number is", a^2)  => Not valid in Python
