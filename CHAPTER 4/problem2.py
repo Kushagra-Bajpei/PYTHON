362f245ee29fcba3b89e5507b9917cdca5b59882
Kushagra Bajpei <kushagrabajpeimbic@gmail.com>
2025-12-02 20:51:27 +0530

    send

---
+++ CHAPTER 4/problem2.py
@@ -0,0 +1,17 @@
+marks = []
+
+l1 = input("Enter marks of student 1: ")
+marks.append(l1)
+l2 = input("Enter marks of student 2: ")
+marks.append(l2)
+l3 = input("Enter marks of student 3: ")
+marks.append(l3)
+l4 = input("Enter marks of student 4: ")
+marks.append(l1)
+l5 = input("Enter marks of student 5: ")
+marks.append(l1)
+l6 = input("Enter marks of student 6: ")
+marks.append(l6)
+
+marks.sort()
+print(marks)
