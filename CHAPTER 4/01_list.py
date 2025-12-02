6236a1bc9df9b1d37c657627c8bdf2e808f7574f
Kushagra Bajpei <kushagrabajpeimbic@gmail.com>
2025-12-02 20:30:10 +0530

    send

---
+++ CHAPTER 4/01_list.py
@@ -0,0 +1,10 @@
+# List are the container to store a set of values of any data type
+# List are mutable
+
+friends = ["Apple", "Orange", 5, 345.06, False, "Aakash", "Rohan"]
+
+print(friends[0])
+friends[0] = "Grapes"  # Unlike Strings lists are mutable
+
+print(friends[0])
+print(friends[1:4])
---
+++ CHAPTER 4/02_list_methods.py
@@ -0,0 +1,13 @@
+friends = ["Apple", "Orange", 5, 345.06, False, "Aakash", "Rohan"]
+print(friends)
+friends.append("Harry")
+print(friends)
+
+l1 =[1, 74, 23, 20, 12]
+# l1.sort()
+# l1.reverse()
+l1.insert(2, 3333)           # Insert 3333 such that its index in the list is 3
+
+value = l1.pop(3)
+print(value)
+print(l1)
